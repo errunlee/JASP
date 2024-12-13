19ffdd729e4dfca67b0e32b39543a690cf2a1016
meV3cT0R <shresthasumit09876@gmail.com>
2024-12-13 11:17:34 +0545

    backend initialization

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import MainRouter from './routes/0.index';
+
+const app = express();
+app.use(express.json());    
+
+const route = express.Router();
+route.use('/api', MainRouter);
+
+require("./config/configs");
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
