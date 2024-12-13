220e9764e34985ad330c7e73171ae39fcaca7929
Jivan Kadel <jivankadel256@gmail.com>
2024-12-13 12:49:22 +0545

    Added store route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import Layout from '@/components/layout/Layout';
 import { BlogRoutes } from '@/modules/blogs';
 import Homepage from '@/modules/Homepage';
-=======
-import Layout from "@/components/layout/Layout";
-import { BlogRoutes } from "@/modules/blogs";
-import Homepage from "@/modules/Homepage";
->>>>>>> 9bde3f86b6c8ff428ce3fa27f3d525143ae57c00
+import { StoreRoutes } from '@/modules/store';
+import { TutorialsRoutes } from '@/modules/tutorials';
 // import LoginForm from "@/modules/auth/Login";
 // import RegisterForm from "@/modules/auth/Register";
 // import { BlogRoutes } from "@/modules/blogs";
@@ -35,32 +31,32 @@ const routes = [
 				element: <p>Login</p>
 			},
 
-      {
-        path: "/register",
-        element: <p>register</p>,
-      },
-      {
-        path: "/blogs",
-        element: <Outlet />,
-        children: BlogRoutes, // BlogRoutes,
-      },
-      {
-        path: "/tutorials",
-        element: <Outlet />,
-        children: [], // TutorialsRoutes,
-      },
-      {
-        path: "/store",
-        element: <Outlet />,
-        children: [], // StoreRoutes,
-      },
-      {
-        path: "/campaigns",
-        element: <Outlet />,
-        children: [], // CampaignRoutes,
-      },
-    ],
-  },
+			{
+				path: '/register',
+				element: <p>register</p>
+			},
+			{
+				path: '/blogs',
+				element: <Outlet />,
+				children: BlogRoutes // BlogRoutes,
+			},
+			{
+				path: '/tutorials',
+				element: <Outlet />,
+				children: TutorialsRoutes // TutorialsRoutes,
+			},
+			{
+				path: '/store',
+				element: <Outlet />,
+				children: StoreRoutes // StoreRoutes,
+			},
+			{
+				path: '/campaigns',
+				element: <Outlet />,
+				children: [] // CampaignRoutes,
+			}
+		]
+	}
 ];
 
 export const router = createBrowserRouter(routes);
