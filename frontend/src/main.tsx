71f23ae9061f70af720bd713dbaf76f57bab8c8d
Arun Khatri <akcs4444@gmail.com>
2024-12-14 23:34:25 +0545

    make lightmode default

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <App />
       </ThemeProvider>
     </QueryClientProvider>
